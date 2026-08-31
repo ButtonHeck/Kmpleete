9b015d9bfa5339985176ab46720ab17f049bdfe6
Ilya Malgin <Soundmaker1505@gmail.com>
2026-08-31 09:53:40 +0300

    application_events.h added

---
+++ src/Libraries/Engine/include/Kmplete/Event/application_events.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "Kmplete/Event/event.h"
+#include "Kmplete/Localization/localization_base.h"
+#include "Kmplete/Utils/string_utils.h"
+
+
+namespace Kmplete
+{
+    //! Definitions of application events supported by engine
+
+    namespace Events
+    {
+        static constexpr auto LocaleChangeEventTypeStr = "LocaleChangeEvent";
+
+        static constexpr auto LocaleChangeEventTypeID = ToStringID(LocaleChangeEventTypeStr);
+
+
+        struct LocaleChangeEvent : public Event
+        {
+            EVENT_CLASS_TYPE(LocaleChangeEventTypeStr)
+
+            explicit LocaleChangeEvent(const LocaleStr& locale) noexcept
+                : _locale(locale)
+            {}
+
+            KMP_NODISCARD const LocaleStr& GetLocale() const noexcept
+            {
+                return _locale;
+            }
+
+            KMP_NODISCARD String ToString() const override
+            {
+                return Utils::Concatenate("LocaleChangeEvent: ", _locale);
+            }
+
+        private:
+            const LocaleStr _locale;
+        };
+        //--------------------------------------------------------------------------
+    }
+}
